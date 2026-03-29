96661e3b8d2f22e02f062ca53753ff39ff15376e
Debnath Hati <debnathhati5000@gmail.com>
2026-03-30 00:31:23 +0530

    Update React and React-DOM to version 19.2.4; adjust Vite build chunk size warning limit

--- vite.config.js
+++ vite.config.js
@@ -13,4 +13,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1000, // Increased chunk size warning limit to 1000 kB
+  },
 });
